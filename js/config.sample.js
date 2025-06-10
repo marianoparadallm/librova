d1d6cb3bd1224a204f4b7119965cbf6aae9fc2b1
marianoparadallm <mariano.parada.llm@gmail.com>
2025-06-10 07:15:24 -0300

    refactor: load supabase credentials from env

---
+++ js/config.sample.js
@@ -0,0 +1,4 @@
+// js/config.sample.js
+// Copia este archivo a config.js y completa tus credenciales
+window.SUPABASE_URL = '<TU_SUPABASE_URL>';
+window.SUPABASE_ANON_KEY = '<TU_SUPABASE_ANON_KEY>';
